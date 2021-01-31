3550c7eee35aaef539b19b211be86396855b4a4b
Korywon <22848944+Korywon@users.noreply.github.com>
2021-01-31 12:43:03 -0600

    Fix formatting of App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Controller from './components/controller.component';
 import GitHub from './components/github.component';
-import { FaRocket } from 'react-icons/fa'
+import { FaRocket } from 'react-icons/fa';
 
 function App() {
   return (
